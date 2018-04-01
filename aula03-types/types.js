b927c2c4dae7128a312686865e9069b23c68730b
mychelrenno <mychelrenno@gmail.com>
2018-04-01 19:13:24 -0300

    aula de interfaces

--- aula03-types/types.js
+++ aula03-types/types.js
@@ -2,4 +2,14 @@ function log(message) {
     console.log(message);
 }
 log('Hello world TypeScript');
-// log(500);
+var teste = ['olá', 'teste'];
+var isActive = true;
+var teste2 = ['teste1', 'teste2'];
+// log(52);
+/*
+string
+number
+boolean
+arrays
+*/ 
+//# sourceMappingURL=types.js.map
